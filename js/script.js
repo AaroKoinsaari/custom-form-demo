030ee8f88a74d6d5655caa6fb9526de91fe05226
Aaro Koinsaari <aaro.koinsaari@proton.me>
2024-02-05 16:01:05 +0200

    Add remove button and value field

--- js/script.js
+++ js/script.js
@@ -6,13 +6,31 @@ $(document).ready(function () {
 
         fieldCount++;
         const div = $('<div>').addClass('field-wrapper').attr('id', `field-${fieldCount}`);
-        const input = $('<input>').attr({
+        const inputHeader = $('<input>').attr({
             type: 'text',
-            name: `text-${fieldCount}`,
-            placeholder: `Text field ${fieldCount}`
+            name: `header-text-${fieldCount}`,
+            placeholder: `Header ${fieldCount}`
         });
 
-        div.append(input);
+        const inputValue = $('<input>').attr({
+            type: 'text',
+            name: `value-text-${fieldCount}`,
+            placeholder: `Value ${fieldCount}`
+        });
+
+        const removeButton = $('<button>')
+            .attr('type', 'button')
+            .addClass('remove-field')
+            .text('Remove')
+            .data('id', fieldCount);
+
+        div.append(inputHeader, inputValue, removeButton);
         $('#form-container').append(div);
     })
+
+    $('#form-container').on('click', '.remove-field', function () {
+        const fieldId = $(this).data('id');
+        $(`#field-${fieldId}`).remove();
+        // TODO: update the field count
+    });
 });
